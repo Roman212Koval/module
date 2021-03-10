f3fe9a62d7a0bae8dee8f001c8e9eda75d32fd01
Roman212Koval <kovalchukroman212@gmail.com>
2021-03-10 22:39:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//const min = require('./min');
+//const sum = require('./summ');
+//const abc = require('./abc');
+var ind = require('./math/index.js');
+
+var array = new Array();
+array = [8,2,9,-11,-2,0];
+
+console.log(ind.min(array));
+console.log(ind.summ(array));
+console.log(ind.abc(array[3]));
